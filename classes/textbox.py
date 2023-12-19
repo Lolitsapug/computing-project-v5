f75620b263183d075ba4fb00ca0b1d1d8c899354
Lolitsapug <daviddeng267@gmail.com>
2023-12-19 20:30:03 +0000

    comments

--- classes/textbox.py
+++ classes/textbox.py
@@ -16,13 +16,13 @@ class Text():
             elif event.key != pygame.K_RETURN:
                 self.value += event.unicode #grabs the unicode letter for the button pressed
 
-            self.text = self.font.render(f"name:{self.value}",True,(255,255,0))
+            self.text = self.font.render(f"name:{self.value}",True,(255,255,0)) #re-renders new text and sets back to default colour (yellow)
 
-            self.rect.width = self.text.get_width() + 10
+            self.rect.width = self.text.get_width() + 10 #resizes the outer bordering rectangle
 
     def draw(self,screen):
-        pygame.draw.rect(screen, (0,0,0), self.rect, 3)
-        screen.blit(self.text,(self.rect.x+5,self.rect.y+5))
+        pygame.draw.rect(screen, (0,0,0), self.rect, 3)       #draws bordering rectangle
+        screen.blit(self.text,(self.rect.x+5,self.rect.y+5))  #draws text
 
         font = pygame.font.SysFont('freesanbold.ttf', 30)
         text = font.render("press enter to set name",True,(255,255,0))
@@ -32,4 +32,5 @@ class Text():
         return self.value
     
     def setGreen(self):
-        self.text = self.font.render(f"name:{self.value}",True,(0,255,0))
+        #sets text green for visual confirmation of user creation
+        self.text = self.font.render(f"name:{self.value}",True,(0,255,0)) 
